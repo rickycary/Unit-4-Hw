b95e92b90f326d7d6167e19226a45609bdd687fc
Richard <ricky.cary1@gmail.com>
2023-02-07 16:23:12 -0600

    landscaper: check stats function complete

--- landscaper/landscaper.py
+++ landscaper/landscaper.py
@@ -11,3 +11,17 @@ tools = [
     {"name": "Fancy Battery Powered Lawnmower", "profit": 100, "cost": 250},
     {"name": "Team of Starving Student", "profit": 250, "cost": 500}  
 ]
+
+## Game Option Functions
+
+def cutGrass():
+    tool = tools[game["tool"]]
+    print(f'You cut the grass with you {tool["name"]} and make ${tool["profit"]}')
+    game["money"] += tool["profit"]   
+    
+def check_stats():
+    tool = tools[game["tool"]]
+    print (f"You currently have {game['money']} and are using a {tool['name']}")
+    
+def upgrade():
+    
