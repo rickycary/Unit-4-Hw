6b25af8f4308d5aebb7de3b262b9cb8a13530895
Richard <ricky.cary1@gmail.com>
2023-02-07 13:44:43 -0600

    landscaper: user can use a team to cut grass

--- landscaper/landscaper.py
+++ landscaper/landscaper.py
@@ -8,6 +8,6 @@ tools = [
     {"name": "Teeth", "profit": 1, "cost": 0},
     {"name": "Pair of Rusty Scissors", "profit": 5, "cost": 5},
     {"name": "Old-Timey Pushy Lawnmower", "profit": 50, "cost": 25},  
-    {"name": "Fancy Battery Powered Lawnmower", "profit": 100, "cost": 250}  
-  
+    {"name": "Fancy Battery Powered Lawnmower", "profit": 100, "cost": 250},
+    {"name": "Team of Starving Student", "profit": 250, "cost": 500}  
 ]
