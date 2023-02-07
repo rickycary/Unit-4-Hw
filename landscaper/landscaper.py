cf48827cbe9470f892cd01107e73d2ae2ee1ac27
Richard <ricky.cary1@gmail.com>
2023-02-07 13:42:49 -0600

    landscaper: user can buy battery-powered lawnmower

--- landscaper/landscaper.py
+++ landscaper/landscaper.py
@@ -7,5 +7,7 @@ game = {"tool": 0, "money": 0}
 tools = [
     {"name": "Teeth", "profit": 1, "cost": 0},
     {"name": "Pair of Rusty Scissors", "profit": 5, "cost": 5},
-    {"name": "Old-Timey Pushy Lawnmower", "profit": 25, "cost": 25},    
+    {"name": "Old-Timey Pushy Lawnmower", "profit": 50, "cost": 25},  
+    {"name": "Fancy Battery Powered Lawnmower", "profit": 100, "cost": 250}  
+  
 ]
