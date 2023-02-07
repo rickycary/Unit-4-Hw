2c9f96d0ac71fffae63c1300dba57d5aeac4f79f
Richard <ricky.cary1@gmail.com>
2023-02-07 13:40:51 -0600

    landscaper: user can buy a push lawnmower

--- landscaper/landscaper.py
+++ landscaper/landscaper.py
@@ -7,7 +7,5 @@ game = {"tool": 0, "money": 0}
 tools = [
     {"name": "Teeth", "profit": 1, "cost": 0},
     {"name": "Pair of Rusty Scissors", "profit": 5, "cost": 5},
-    {"name": "Teeth", "profit": 1, "cost": 0},
-    {"name": "Teeth", "profit": 1, "cost": 0}
-    
+    {"name": "Old-Timey Pushy Lawnmower", "profit": 25, "cost": 25},    
 ]
